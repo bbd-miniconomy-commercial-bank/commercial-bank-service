60421c4532344c6be6d152bc867b9a70c2fcde84
Ryan Trickett <Ryan.Trickett@standardbank.co.za>
2024-06-22 22:07:09 +0200

    Added Spring Boot Project

---
+++ src/main/java/com/miniconomy/commercial_bank_service/CommercialBankServiceApplication.java
@@ -0,0 +1,13 @@
+package com.miniconomy.commercial_bank_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommercialBankServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommercialBankServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miniconomy/commercial_bank_service/CommercialBankServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miniconomy.commercial_bank_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommercialBankServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
