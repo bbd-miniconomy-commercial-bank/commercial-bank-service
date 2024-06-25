2f1e9fcd7feecf6766df80cf932202d80dbdfed1
devwasabi2 <Wandile.Nyembe@bbd.co.za>
2024-06-25 21:49:33 +0200

    update db schema to match erd

--- sql/debit_order.sql
+++ sql/debit_order.sql
@@ -1,11 +1,11 @@
 CREATE TABLE debitorder (
     id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    sender_acc_id INTEGER NOT NULL,
-    receiver_acc_id INTEGER NOT NULL,
-    sender_ref VARCHAR(50) NOT NULL,
-    receiver_ref VARCHAR(50) NOT NULL,
+    credit_acc_id INTEGER NOT NULL,
+    debit_acc_id INTEGER NOT NULL,
+    db_order_sender_ref VARCHAR(50) NOT NULL,
+    db_order_receiver_ref VARCHAR(50) NOT NULL,
     db_order_amount NUMERIC(10, 2) NOT NULL,
     db_order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (sender_acc_id) REFERENCES account(id) ON DELETE CASCADE,
-    FOREIGN KEY (receiver_acc_id) REFERENCES account(id) ON DELETE CASCADE
+    FOREIGN KEY (credit_acc_id) REFERENCES account(id) ON DELETE CASCADE,
+    FOREIGN KEY (debit_acc_id) REFERENCES account(id) ON DELETE CASCADE
 );
--- sql/transaction.sql
+++ sql/transaction.sql
@@ -2,9 +2,13 @@ CREATE TYPE transaction_status_enum AS ENUM ('pending', 'completed');
 
 CREATE TABLE transaction (
     id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    account_id INTEGER NOT NULL,
-    transaction_status transaction_status_enum NOT NULL,
+    credit_acc_id INTEGER NOT NULL,
+    debit_acc_id INTEGER NOT NULL,
+    transaction_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     transaction_amount NUMERIC(10, 2) NOT NULL,
-    transaction_ref VARCHAR(50) NOT NULL,
-    FOREIGN KEY (account_id) REFERENCES account(id) ON DELETE CASCADE
+    credit_ref VARCHAR(50) NOT NULL,
+    debit_ref VARCHAR(50) NOT NULL,
+    transaction_status transaction_status_enum NOT NULL,
+    FOREIGN KEY (credit_acc_id) REFERENCES account(id) ON DELETE CASCADE,
+    FOREIGN KEY (debit_acc_id) REFERENCES account(id) ON DELETE CASCADE
 );
