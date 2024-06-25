53a612e84b16bbfa8e9292ba1847cb92693850d8
devwasabi2 <Wandile.Nyembe@bbd.co.za>
2024-06-25 21:38:33 +0200

    added enum for loan type for loan table

--- sql/loan.sql
+++ sql/loan.sql
@@ -1,8 +1,9 @@
+CREATE TYPE loan_type_enum AS ENUM ('short-term', 'long-term');
+
 CREATE TABLE loan (
     id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     account_id INTEGER NOT NULL,
-    debit_order_id INTEGER NOT NULL,
     loan_amount NUMERIC(10, 2) NOT NULL,
-    FOREIGN KEY (account_id) REFERENCES account(id) ON DELETE CASCADE,
-    FOREIGN KEY (debit_order_id) REFERENCES debitorder(id) ON DELETE CASCADE
+    loan_type loan_type_enum NOT NULL,
+    FOREIGN KEY (account_id) REFERENCES account(id) ON DELETE CASCADE
 );
