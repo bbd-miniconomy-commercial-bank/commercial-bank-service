e3a7f57795473a98877965ee459c584b6504933b
devwasabi2 <Wandile.Nyembe@bbd.co.za>
2024-06-26 22:03:42 +0200

    added credit_total view in account_balance sql script

--- sql/account_balance.sql
+++ sql/account_balance.sql
@@ -1,7 +1,7 @@
 -- liquibase formatted sql
 
 -- changeset devwasabi2:create-debit-account-transactions-total-view
-CREATE VIEW debit_account_balance AS
+CREATE VIEW debit_account_total AS
 SELECT 
     debit_account_id,
     SUM(transaction_amount) AS total_amount
@@ -9,4 +9,15 @@ FROM
     transaction
 GROUP BY 
     debit_account_id;
--- rollback DROP VIEW debit_account_balance;
+-- rollback DROP VIEW debit_account_total;
+
+-- changeset devwasabi2:create-credit-account-transactions-total-view
+CREATE VIEW credit_account_total AS
+SELECT 
+    credit_account_id,
+    SUM(transaction_amount) AS total_amount
+FROM 
+    transaction
+GROUP BY 
+    credit_account_id;
+-- rollback DROP VIEW credit_account_total;
