952f6f5121d59e7c7236c1302552ff45565acb56
devwasabi2 <Wandile.Nyembe@bbd.co.za>
2024-06-30 15:13:12 +0200

    created account delegation model class

--- src/main/java/com/miniconomy/commercial_bank_service/financial_management/service/AccountService.java
+++ src/main/java/com/miniconomy/commercial_bank_service/financial_management/service/AccountService.java
@@ -5,16 +5,20 @@ import java.util.Optional;
 import org.springframework.stereotype.Service;
 
 import com.miniconomy.commercial_bank_service.financial_management.entity.Account;
+import com.miniconomy.commercial_bank_service.financial_management.entity.AccountDelegation;
+import com.miniconomy.commercial_bank_service.financial_management.repository.AccountDelegationRepository;
 import com.miniconomy.commercial_bank_service.financial_management.repository.AccountRepository;
 
 @Service
 public class AccountService
 {
   private final AccountRepository accountRepository;
+  private final AccountDelegationRepository accDelegationRepo;
 
-  public AccountService(AccountRepository accountRepository)
+  public AccountService(AccountRepository accountRepository, AccountDelegationRepository acdR)
   {
     this.accountRepository = accountRepository;
+    this.accDelegationRepo = acdR;
   }
   
   public Optional<Account> retrieveAccountBalance(String accountName)
