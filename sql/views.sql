3f046fca71eb70de0bdb87ccdd678526162d6b77
ThashilBBD <thashil.naidoo@bbd.co.za>
2024-06-30 11:30:26 +0200

    Added view for all transactions associated with a debit order

--- sql/views.sql
+++ sql/views.sql
@@ -21,3 +21,26 @@ FROM
 GROUP BY 
     credit_account_id;
 -- rollback DROP VIEW IF EXISTS credit_account_total;
+
+-- changeset thashilbbd:create-debit-order-transactions-view
+CREATE VIEW debit_order_transactions AS
+SELECT 
+    dot.debit_order_transaction_id,
+    dot.debit_order_id,
+    dot.transaction_id,
+    "do".credit_account_id,
+    credit_account.account_name AS credit_account_name,
+    "do".debit_account_id,
+    debit_account.account_name AS debit_account_name,
+    t.transaction_date,
+    t.transaction_amount,
+    t.credit_ref,
+    t.debit_ref,
+    t.transaction_status
+FROM 
+    debit_order_transaction dot
+    JOIN debit_order "do" ON dot.debit_order_id = "do".debit_order_id
+    JOIN transaction t ON dot.transaction_id = t.transaction_id
+    JOIN account credit_account ON "do".credit_account_id = credit_account.account_id
+    JOIN account debit_account ON "do".debit_account_id = debit_account.account_id;
+-- rollback DROP VIEW debit_order_transactions
