e3d728f5c8f36047361ebb597d1efea0180c7930
Ryan Trickett <Ryan.Trickett@standardbank.co.za>
2024-07-02 22:44:56 +0200

    Fixed compilation issues

--- src/main/java/com/miniconomy/commercial_bank_service/financial_management/adapter/RetailBankAdapter.java
+++ src/main/java/com/miniconomy/commercial_bank_service/financial_management/adapter/RetailBankAdapter.java
@@ -2,12 +2,14 @@ package com.miniconomy.commercial_bank_service.financial_management.adapter;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import com.miniconomy.commercial_bank_service.financial_management.entity.OutgoingInterbankDeposit;
 import com.miniconomy.commercial_bank_service.financial_management.request.OutgoingRetailBankRequest;
 import com.miniconomy.commercial_bank_service.financial_management.utils.InterbankUtils;
 
+@Service
 public class RetailBankAdapter implements InterbankAdapter {
 
     private final RestTemplate restTemplate;
