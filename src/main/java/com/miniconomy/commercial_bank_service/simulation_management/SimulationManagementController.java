85bbb36ec65e8c55600df8f291453b04617ab062
tawanda-bbd <156799248+tawanda-bbd@users.noreply.github.com>
2024-07-03 11:35:44 +0200

    Update

--- src/main/java/com/miniconomy/commercial_bank_service/simulation_management/SimulationManagementController.java
+++ src/main/java/com/miniconomy/commercial_bank_service/simulation_management/SimulationManagementController.java
@@ -3,7 +3,7 @@ package com.miniconomy.commercial_bank_service.simulation_management;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.miniconomy.commercial_bank_service.financial_management.response.ResponseTemplate;
@@ -23,18 +23,21 @@ public class SimulationManagementController {
     }
 
     @Operation(
-        summary = "Get Hand of Zeus Time",
-        description = "Get the current time"
+        summary = "Reset the Simulation",
+        description = "Reset the CommercialBank"
     )
-    @GetMapping(value = "/getdate", produces = "application/json")
+    @PostMapping(value = "/reset", produces = "application/json")
     public ResponseEntity<ResponseTemplate<String>> getCurrentDate() {
         ResponseTemplate<String> response = new ResponseTemplate<>();
         int status = HttpStatus.OK.value();
 
         try {
+
             String currentDate = simulationService.getSimulationDate();
+
+            
             response.setData(currentDate);
-            response.setMessage("Current date retrieved successfully");
+            response.setMessage("Successfully reset commercial Bank");
         } catch (Exception e) {
             status = HttpStatus.INTERNAL_SERVER_ERROR.value();
             response.setMessage("Internal Server Error: " + e.getMessage());
@@ -45,5 +48,5 @@ public class SimulationManagementController {
     }
 
     //resetiing / starting asap
-    
+
 }
---
+++ src/main/java/com/miniconomy/commercial_bank_service/simulation_management/SimulationManagementRepository.java
@@ -0,0 +1,88 @@
+package com.miniconomy.commercial_bank_service.simulation_management;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
+public class SimulationManagementRepository {
+
+    @Autowired
+    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    private void deleteAllAccounts() {
+        String sql = "DELETE FROM account";
+
+        namedParameterJdbcTemplate.getJdbcOperations().update(sql);
+
+    }
+
+    private void deleteAllAccountsDelegation() {
+        String sql = "DELETE FROM account_delegation";
+
+        namedParameterJdbcTemplate.getJdbcOperations().update(sql);
+
+    }
+
+    private void deleteDebitOrderTransactions() {
+        String sql = "DELETE FROM debit_order_transaction";
+
+        namedParameterJdbcTemplate.getJdbcOperations().update(sql);
+
+    }
+
+    private void deleteAllDebitOrders() {
+        String sql = "DELETE FROM debit_order";
+
+        namedParameterJdbcTemplate.getJdbcOperations().update(sql);
+
+    }
+
+    private void deleteAllInterbankTransactions() {
+        String sql = "DELETE FROM interbank_transaction";
+
+        namedParameterJdbcTemplate.getJdbcOperations().update(sql);
+
+    }
+
+    private void deleteAllLoanInterests() {
+        String sql = "DELETE FROM loan_interest";
+
+        namedParameterJdbcTemplate.getJdbcOperations().update(sql);
+
+    }
+
+    private void deleteAllLoanTransactions() {
+        String sql = "DELETE FROM loan_transaction";
+
+        namedParameterJdbcTemplate.getJdbcOperations().update(sql);
+
+    }
+
+    private void deleteAllLoans() {
+        String sql = "DELETE FROM loan";
+
+        namedParameterJdbcTemplate.getJdbcOperations().update(sql);
+
+    }
+    private void deleteAllTransactions() {
+        String sql = "DELETE FROM transaction";
+
+        namedParameterJdbcTemplate.getJdbcOperations().update(sql);
+
+    }
+    
+    public void deleteAllTables() {
+        deleteAllLoanInterests();
+        deleteDebitOrderTransactions();
+        deleteAllInterbankTransactions();
+        deleteAllLoanTransactions();
+        deleteAllTransactions();
+        deleteAllDebitOrders();
+        deleteAllLoans();
+        deleteAllAccountsDelegation();
+        deleteAllAccounts();
+
+     
+    }
+      
+}
